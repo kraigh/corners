ca99345600f265037a76c2ef4e8fe40d2b178b72
Kraig Hufstedler <kraigory@gmail.com>
2012-11-09 03:22:23 -0600

    small fix to scrollfixed.js

--- js/scrollfixed.js
+++ js/scrollfixed.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    var theLoc = 590;
+    var theLoc = 546;
     $(window).scroll(function() {
         if(theLoc >= $(window).scrollTop()) {
             $('body').css('background-attachment','scroll');
             $('body').css('background-position','center -1000px');
         } else {
             $('body').css('background-attachment','fixed');
-            $('body').css('background-position','center -1590px');
+            $('body').css('background-position','center -1546px');
 
         }
     });
